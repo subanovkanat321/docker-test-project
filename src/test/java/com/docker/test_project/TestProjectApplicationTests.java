242f39491bda38057c88e195393834e1bebf63fa
kanat <ergeshmametuulukanat@gmail.com>
2025-07-05 16:11:53 +0300

    test and fix docker.yml

--- src/test/java/com/docker/test_project/TestProjectApplicationTests.java
+++ src/test/java/com/docker/test_project/TestProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.docker.test_project;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class TestProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
